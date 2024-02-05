9fb17cdfe9d2bdc503d3a731d1a828b623196f21
rafael <rafael.rmo.123456789@gmail.com>
2024-02-05 16:34:28 -0400

    soma

---
+++ Soma.java
@@ -0,0 +1,11 @@
+public class Soma {
+    public static void main (String args [])
+    {
+        int x = 10;
+        int y = 20;
+        System.out.println(x+y);
+        long z = 40;
+        x = (int) z;
+        System.out.println(x);
+    }
+}
