b493a0800a53fd8d41fb445f2373a69cbd5cd355
Rafael Melchior de Oliveira <118372879+Rafuel05@users.noreply.github.com>
2024-02-06 14:24:44 -0400

    Add files via upload

---
+++ Exercícios_Cap_3/Exerc_5.java
@@ -0,0 +1,11 @@
+public class Exerc_5 {
+    public static void main(String[] args) {
+        long	fatorial	=	1;
+        for	(long	n	=	1;	n	<=	40;	n++)	
+        {
+            fatorial = fatorial * n;
+            System.out.println(fatorial);
+        }
+    }
+    //apos mudar a variavel para long o numero de resultados corretos aumetou, pois o long tem mais espaço de memoria em comparaçao ao int
+}
---
+++ Exercícios_Cap_3/Exerc_7.java
@@ -0,0 +1,17 @@
+public class Exerc_7 {
+    public static void main(String[] args) {
+        int x = 80;
+        System.out.print(x);
+        while (x!=1) {
+            
+           if ( x % 2 == 0 ){
+            
+                x = x/2;
+           }
+           else{
+            x	=	3	*	x	+	1;
+           }
+           System.out.print(" ->"+x);
+        }
+    }
+}
