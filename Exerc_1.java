132ee37ae95b52ef19f38c1dba14ccd9a1763067
Rafael Melchior de Oliveira <118372879+Rafuel05@users.noreply.github.com>
2024-02-06 14:23:51 -0400

    Delete Exerc_1.java

--- Exerc_1.java
+++
@@ -1,9 +0,0 @@
-public class Exerc_1 {
-    public static void main ( String args [])
-    {
-        for (int i = 150; i != 301; i++)
-        {
-            System.out.println(i);
-        }
-    }
-}
